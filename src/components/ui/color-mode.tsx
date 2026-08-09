ff9cdcaa9dc464f49afaa3ce7197dd123b5d0c8b
manit <manit17102006@gmail.com>
2026-08-09 12:56:20 +0530

    more products added

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -2,8 +2,8 @@
 
 import type { IconButtonProps, SpanProps } from "@chakra-ui/react"
 import { ClientOnly, IconButton, Skeleton, Span } from "@chakra-ui/react"
-import { ThemeProvider, useTheme } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+import { ClientThemeProvider as ThemeProvider, useTheme } from "@wrksz/themes/client"
+import type { ThemeProviderProps } from "@wrksz/themes/client"
 import * as React from "react"
 import { LuMoon, LuSun } from "react-icons/lu"
 
